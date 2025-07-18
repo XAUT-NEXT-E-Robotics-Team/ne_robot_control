b8169cb1c6f82459de54d1794c085c028088bda6
imgzw <guziwen888@163.com>
2025-07-18 11:43:27 +0800

    添加了裁判系统相关文件，调整了算法库在项目中位置

---
+++ chassis/modules/referee/referee.h
@@ -0,0 +1,23 @@
+#ifndef REFEREE_H
+#define REFEREE_H
+
+#include "main.h"
+//#include "CRC8_CRC16.h"
+#include "fifo.h"
+#include "main.h"
+#include "protocol.h"
+// #include "refereeData_v1.4.h"
+// #include "refereeData_v1.5.h"
+// #include "refereeData_v1.6.h"
+#include "refereeData_v1.7.h"
+#include "stdio.h"
+#include "string.h"
+#include "tim.h"
+
+#define USART_RX_BUF_LENGHT     512
+#define REFEREE_FIFO_BUF_LENGTH 1024
+extern void refereeINIT(void);
+extern void referee_unpack_fifo_data(void);
+extern void refereeReceiveHandler(void);
+
+#endif
